45a16f945e527e0d517cb1d86870c1e1369ecf94
Alexander Gaydardzhiev <alexander.gaydardzhiev@zautre.com>
2017-04-28 14:09:02 +0300

    KLARNAPII-104 Guest Customer can not place order on IE and Edge browsers

--- cartridges/int_klarna_payments/cartridge/static/default/js/klarna-payments.js
+++ cartridges/int_klarna_payments/cartridge/static/default/js/klarna-payments.js
@@ -4,19 +4,20 @@
 	var klarnaPaymentsObjects = window.KlarnaPaymentsObjects;
 	
 	//billing address form required fields
-	var $firstName 			= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_addressFields_firstName"]')[0];
-	var $lastName 			= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_addressFields_lastName"]')[0];
-	var $address1 			= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_addressFields_address1"]')[0];
-	var $city 				= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_addressFields_city"]')[0];
-	var $postal 			= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_addressFields_postal"]')[0];
-	var $country 			= document.querySelectorAll('select[name="dwfrm_billing_billingAddress_addressFields_country"]')[0];
-	var $state 				= document.getElementsByName('dwfrm_billing_billingAddress_addressFields_states_state')[0];
-	var $phone 				= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_addressFields_phone"]')[0];
-	var $emailAddress 		= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_email_emailAddress"]')[0];
-	var $billingAddressForm = document.querySelectorAll('#dwfrm_billing > fieldset')[0];	
-	var $authorizationToken	= document.querySelectorAll('input[name="klarna_payments_authorization_token"]')[0];
-	var $klarnaPI			= document.getElementById('is-Klarna');
-	var $continueBtn		= document.getElementsByName('dwfrm_billing_save')[1];
+	var $firstName 					= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_addressFields_firstName"]')[0];
+	var $lastName 					= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_addressFields_lastName"]')[0];
+	var $address1 					= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_addressFields_address1"]')[0];
+	var $city 						= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_addressFields_city"]')[0];
+	var $postal 					= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_addressFields_postal"]')[0];
+	var $country 					= document.querySelectorAll('select[name="dwfrm_billing_billingAddress_addressFields_country"]')[0];
+	var $state 						= document.getElementsByName('dwfrm_billing_billingAddress_addressFields_states_state')[0];
+	var $phone 						= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_addressFields_phone"]')[0];
+	var $emailAddress 				= document.querySelectorAll('input[name="dwfrm_billing_billingAddress_email_emailAddress"]')[0];
+	var $billingAddressFormElements = document.querySelectorAll('input[name*=dwfrm_billing_billingAddress], select[name*=dwfrm_billing_billingAddress]');
+	var $billingAddressForm 		= document.querySelectorAll('#dwfrm_billing > fieldset')[0];	
+	var $authorizationToken			= document.querySelectorAll('input[name="klarna_payments_authorization_token"]')[0];
+	var $klarnaPI					= document.getElementById('is-Klarna');
+	var $continueBtn				= document.getElementsByName('dwfrm_billing_save')[1];
 	
 	
 	window.klarnaAsyncCallback = function ()
@@ -35,7 +36,7 @@
 
 	};
 	$billingAddressForm.addEventListener( "click", function () {
-		var elements = $billingAddressForm.elements;
+		var elements = $billingAddressFormElements;
 		var formValid = true;
 		for (i=0; i<elements.length; i++)
 		{
@@ -51,7 +52,7 @@
 		}		
 	})
 	$klarnaPI.addEventListener( "click", function () {
-		var elements = $billingAddressForm.elements;
+		var elements = $billingAddressFormElements;
 		var formValid = true;
 		for (i=0; i<elements.length; i++)
 		{

f5e6609272fba0d6b09db967915c2be7565a6b8f
Ivan Zanev <ivan.zanev@tryzens.com>
2019-04-25 13:19:40 +0300

    - add missing multishipping merchant data code from commit 5cf0aef and fix for shipments with null address.

--- cartridges/int_klarna_payments_sfra/cartridge/scripts/klarna_payments/requestBuilder/shipmentItem.js
+++ cartridges/int_klarna_payments_sfra/cartridge/scripts/klarna_payments/requestBuilder/shipmentItem.js
@@ -7,7 +7,7 @@ var ShippingLocation = require('dw/order/ShippingLocation');
 
 var Builder = require('*/cartridge/scripts/klarna_payments/Builder');
 var LineItem = require('*/cartridge/scripts/klarna_payments/model/request/session').LineItem;
-
+var AddressRequestBuilder = require('*/cartridge/scripts/klarna_payments/requestBuilder/address');
 var isTaxationPolicyNet = require('*/cartridge/scripts/util/KlarnaUtils').isTaxationPolicyNet;
 
 var ORDER_LINE_TYPE = require('*/cartridge/scripts/util/KlarnaPaymentsConstants.js').ORDER_LINE_TYPE;
@@ -17,10 +17,14 @@ var ORDER_LINE_TYPE = require('*/cartridge/scripts/util/KlarnaPaymentsConstants.
  */
 function ShipmentItem() {
     this.item = null;
+    this.addressRequestBuilder = new AddressRequestBuilder();
 }
 
 ShipmentItem.prototype = new Builder();
 
+ShipmentItem.prototype.getAddressRequestBuilder = function () {
+    return this.addressRequestBuilder;
+};
 ShipmentItem.prototype.calculateShippingTotalTaxAmount = function (shipment) {
     return (isTaxationPolicyNet()) ? 0 : Math.round(shipment.shippingTotalTax.value * 100);
 };
@@ -50,6 +54,33 @@ ShipmentItem.prototype.getShipmentUnitPrice = function (shipment) {
     return shipmentUnitPrice;
 };
 
+ShipmentItem.prototype.getShipmentProductIds = function (shipment) {
+    var productIds = [];
+
+    var shipmentLineItemsIterator = shipment.getProductLineItems().iterator();
+
+    while (shipmentLineItemsIterator.hasNext()) {
+        var shipmentLineItem = shipmentLineItemsIterator.next();
+
+        productIds.push(shipmentLineItem.productID);
+    }
+
+    return productIds;
+};
+
+ShipmentItem.prototype.buildMerchantData = function (shipment) {
+    var merchantDataStr = JSON.stringify({
+        'products': this.getShipmentProductIds(shipment),
+        'address': this.getAddressRequestBuilder().buildFlat(shipment.shippingAddress)
+    });
+
+    if (merchantDataStr.length > 255) {
+        merchantDataStr = merchantDataStr.substr(0, 255-3) + '...';
+    }
+
+    return merchantDataStr;
+};
+
 ShipmentItem.prototype.build = function (shipment) {
     var shipmentTaxRate = this.getShipmentTaxRate(shipment);
     var shipmentUnitPrice = Math.round(this.getShipmentUnitPrice(shipment));
@@ -62,6 +93,10 @@ ShipmentItem.prototype.build = function (shipment) {
     this.item.unit_price = shipmentUnitPrice;
     this.item.tax_rate = Math.round(shipmentTaxRate);
     this.item.total_amount = shipmentUnitPrice;
+
+    if (!empty(shipment.shippingAddress)) {
+        this.item.merchant_data = this.buildMerchantData(shipment);
+    }
 
     this.item.total_tax_amount = this.calculateShippingTotalTaxAmount(shipment);
 
